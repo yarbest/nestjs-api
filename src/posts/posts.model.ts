a9c3d677b1b20a9552f2ceab3e9ed77ef07d5f65
yarbest <yarbest15@gmail.com>
2025-01-29 13:26:16 +0200

    add posts model

---
+++ src/posts/posts.model.ts
@@ -0,0 +1,52 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { UsersModel } from 'src/users/users.model';
+
+interface PostCreationAttrs {
+  title: string;
+  content: string;
+  userId: number;
+  imageSrc: string;
+}
+
+@Table({
+  tableName: 'posts',
+})
+export class PostsModel extends Model<PostsModel, PostCreationAttrs> {
+  @Column({
+    primaryKey: true,
+    unique: true,
+    type: DataType.INTEGER,
+    autoIncrement: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  title: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  content: string;
+
+  @Column({ type: DataType.STRING })
+  imageSrc: string;
+
+  @ForeignKey(() => UsersModel)
+  @Column({ type: DataType.INTEGER })
+  userId: number;
+
+  @BelongsTo(() => UsersModel)
+  author: UsersModel;
+}
